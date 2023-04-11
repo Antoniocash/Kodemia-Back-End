5e1b2455e5292c6fb47805198173c9050d79397f
AntonioBatisa <antoniobatistae@gmail.com>
2023-04-10 22:51:32 -0400

    modified promises practice file

--- sesion-5-promesas/promesas.js
+++ sesion-5-promesas/promesas.js
@@ -1,4 +1,4 @@
-const myFirstPromise = new Promise((resolve, reject) => {
+/* const myFirstPromise = new Promise((resolve, reject) => {
 
     setTimeout(() => {
         let error = null
@@ -15,6 +15,76 @@ console.log(myFirstPromise)
 myFirstPromise
 .then((result) => {
     console.log(result)
+})
+.catch((error) => {
+    console.log(error)
+}) */
+
+const person = {
+    name: "Antonio",
+    atSuperMarket: false,
+    paidPantry: false,
+    atHome: false
+}
+
+function goToSuperMarket(personInSuperMarket){
+    return new Promise((resolve, reject) => {
+        console.log(`${personInSuperMarket.name} esta yendo al Chedrahui`)
+        setTimeout(()=>{
+            personInSuperMarket.atSuperMarket = true
+            if(!personInSuperMarket.atSuperMarket){
+                reject(`${personInSuperMarket} fue asaltado en el camino.`)
+            }
+            resolve(personInSuperMarket)
+        }, 3000)
+    }) 
+}
+
+function pay(personAtCashier){
+    return new Promise((resolve, reject) => {
+        console.log(`${personAtCashier.name} esta pagando sus articulos.`)
+        setTimeout(()=>{
+            personAtCashier.paidPantry = true
+            if(!personAtCashier.paidPantry){
+                reject(`${personAtCashier.name} fallecio en el intento.`)
+            }
+            resolve(personAtCashier)
+        }, 5000)
+    }) 
+}
+
+function goHome(personAtHome){
+    return new Promise((resolve, reject) => {
+        console.log(`${personAtHome.name} esta yendo a casa.`)
+        setTimeout(()=>{
+            personAtHome.atHome = true
+            if(!personAtHome.atHome){
+                reject(`${personAtHome.name} fue victima de la inseguridad.`)
+            }
+            resolve(personAtHome)
+        }, 7000)
+    }) 
+}
+
+goToSuperMarket(person).then((personInSuperMarket) =>{
+    console.log(`${personInSuperMarket.name} llego al Chedraui.`)
+    console.log(personInSuperMarket)
+
+    pay(personInSuperMarket).then((personAtCashier) =>{
+        console.log(`${personAtCashier.name} ha pagado.`)
+        console.log(personAtCashier)
+        
+        goHome(personAtCashier).then((personAtHome) =>{
+            console.log(`${personAtHome.name} ha llegado a casa.`)
+            console.log(personAtHome)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 })
 .catch((error) => {
     console.log(error)
