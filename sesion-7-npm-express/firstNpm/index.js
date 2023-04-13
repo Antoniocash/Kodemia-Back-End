e64bc4e1b56dd3c74594c596f54a7af3afe063b4
AntonioBatisa <antoniobatistae@gmail.com>
2023-04-12 22:22:56 -0400

    modifed firstNpm file

---
+++ sesion-7-npm-express/firstNpm/index.js
@@ -0,0 +1,22 @@
+import colors from "colors"
+
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+  });
+   
+  // outputs red text
+  console.log("this is an error".error);
+   
+  // outputs yellow text
+  console.log("this is a warning".warn);
+
+console.log("hola koder")
